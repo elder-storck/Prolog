7a4820896920de5a3651fcfd1f307305bd1146ea
elder.storck <84104017+elder-storck@users.noreply.github.com>
2023-09-20 16:16:04 -0300

    Update t1.pl
    update database

--- t1.pl
+++ t1.pl
@@ -7,24 +7,28 @@ PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
 PREFIX dboW: <http://dbpedia.org/ontology/Work/>
 PREFIX foaf: <http://xmlns.com/foaf/0.1/>
 
-SELECT DISTINCT ?title ?minutos ?orcamento ?nomeDir
+SELECT DISTINCT ?db_titulo ?db_ator ?db_produtora ?db_diretor
 WHERE { ?sujeito a  dbo:TelevisionShow ;
-  rdfs:label ?title;
-  dboW:runtime ?tempo;
-            dbo:network ?produtora;
-dbo:director ?diretor
-  OPTIONAL {?sujeito dbo:budget ?orcamento}
+					rdfs:label ?db_titulo;
+     				dboW:runtime ?tempo;
+         			dbo:network ?produtora;
+            		dbo:director ?diretor;
+              		dbo:starring ?estrelas
+  	
+  	OPTIONAL {?sujeito dbo:budget ?orcamento}
  
-  ?produtora rdfs:label ?prodname.
-  ?diretor   rdfs:label ?nomeDir.
+  	?produtora rdfs:label ?db_produtora.
+  	?diretor   rdfs:label ?db_diretor.
+  	?estrelas rdfs:label ?db_ator.
  
-  BIND (xsd:float(?tempo) as ?minutos)
+  	BIND (xsd:float(?tempo) as ?db_duracao)
  
-  FILTER (lang(?title) = 'en')
-  FILTER (lang(?prodname) = 'en')
-  FILTER (lang(?nomeDir) = 'en')
+  	FILTER (lang(?db_ator) = 'en')
+  	FILTER (lang(?db_titulo) = 'en')
+  	FILTER (lang(?db_produtora) = 'en')
+  	FILTER (lang(?db_diretor) = 'en')
 }
-ORDER BY DESC(?minutos) ?title
+ORDER BY DESC(?db_duracao) ?db_titulo
    ",
    [ endpoint('https://dbpedia.org/sparql')])  ).
 
